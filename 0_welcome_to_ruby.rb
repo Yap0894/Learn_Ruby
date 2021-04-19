2456fd27ca8d5b55a5ad67a31c6a583e4e5a0199
Yap <yapalex1994@gmail.com>
2021-04-20 03:54:44 +0700

    rename file name to snake case

---
+++ 0_welcome_to_ruby.rb
@@ -0,0 +1,27 @@
+# print "Hello World"
+# p "Hello World"
+# puts "Hellow World"
+
+#puts "Hello World"
+
+#greeting = "Hello World2"
+#puts greeting
+
+# def sayHello()
+#     puts "Hellow WOrld3"
+# end
+
+# sayHello
+
+
+# def sayHello(thing_to_say)
+#     puts thing_to_say
+# end
+
+# sayHello "ruby is Great"
+
+first_Name = "Alex"
+last_Name = "Upa"
+
+puts "#{first_Name} #{last_Name}"
+
